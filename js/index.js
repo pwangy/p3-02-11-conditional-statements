f2dc86f19f2d7d3bebf0f33b3d83f462ac9fe7ef
pwangy <blipsandclicks@gmail.com>
2024-03-11 12:29:21 -0700

    complete codealong

--- js/index.js
+++ js/index.js
@@ -4,11 +4,14 @@
   return "Access granted". Otherwise, return "Access denied".
 */
 function adminLogin(username, password) {
-  if ((username === "admin" || username === "ADMIN") && password === "12345") {
-    return "Access granted";
-  } else {
-    return "Access denied";
-  }
+	if (
+		(username === 'admin' || username === 'ADMIN') &&
+		password === '12345'
+	) {
+		return 'Access granted'
+	} else {
+		return 'Access denied'
+	}
 }
 
 /*
@@ -18,17 +21,17 @@ function adminLogin(username, password) {
   return "It's too dang hot out there!". Otherwise, return "It's perfect out there!"
 */
 function howsTheWeather(temperature) {
-  let response;
-  if (temperature < 40) {
-    response = "brisk";
-  } else if (temperature >= 40 && temperature <= 65) {
-    response = "a little chilly";
-  } else if (temperature > 85) {
-    response = "too dang hot";
-  } else {
-    response = "perfect";
-  }
-  return `It's ${response} out there!`;
+	let response
+	if (temperature < 40) {
+		response = 'brisk'
+	} else if (temperature >= 40 && temperature <= 65) {
+		response = 'a little chilly'
+	} else if (temperature > 85) {
+		response = 'too dang hot'
+	} else {
+		response = 'perfect'
+	}
+	return `It's ${response} out there!`
 }
 
 /* 
@@ -38,15 +41,15 @@ function howsTheWeather(temperature) {
   all other numbers, just return the number itself.
 */
 function fizzbuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {
-    return num;
-  }
+	if (num % 3 === 0 && num % 5 === 0) {
+		return 'FizzBuzz'
+	} else if (num % 3 === 0) {
+		return 'Fizz'
+	} else if (num % 5 === 0) {
+		return 'Buzz'
+	} else {
+		return num
+	}
 }
 
 /*
@@ -56,16 +59,16 @@ function fizzbuzz(num) {
   output a message saying "Invalid operation!" and return `nil`.
 */
 function calculator(operation, num1, num2) {
-  switch (operation) {
-    case "+":
-      return num1 + num2;
-    case "-":
-      return num1 - num2;
-    case "*":
-      return num1 * num2;
-    case "/":
-      return num1 / num2;
-    default:
-      console.log("Invalid operation!");
-  }
+	switch (operation) {
+		case '+':
+			return num1 + num2
+		case '-':
+			return num1 - num2
+		case '*':
+			return num1 * num2
+		case '/':
+			return num1 / num2
+		default:
+			console.log('Invalid operation!')
+	}
 }
--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 
-def admin_login(username, password):
-    # your code here
-    pass
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
+# value_if_true if condition else value_if_false
+def admin_login(username, password):
+    if (username == 'admin' or username == 'ADMIN') and password == '12345':
+        return 'Access granted'
+    else:
+        return 'Access denied'
+
+def hows_the_weather(temp):
+    if temp < 40:
+        res = 'brisk'
+    elif temp >= 40 and temp <= 65:
+        res = 'a little chilly'
+    elif temp > 85:
+        res = 'too dang hot'
+    else:
+        res = 'perfect'
+    return "It's "+ res + " out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if (num % 3 == 0 and num % 5 == 0):
+        return 'FizzBuzz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    else:
+        return num
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+def calculator(op, n1, n2):
+    if op == '+':
+        return n1 + n2
+    elif op == '-':
+        return n1 - n2
+    elif op == '*':
+        return n1 * n2
+    elif op == '/':
+        return n1 / n2
+    else:
+        print('Invalid operation!')
+        return None
